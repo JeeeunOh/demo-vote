33969c757dbec4c4ae06e3c4c708dce7b6764f1a
jieunOh <0909oje@naver.com>
2022-12-21 22:17:22 +0900

    :art: 새 프로젝트

---
+++ app/src/index.tsx
@@ -0,0 +1,23 @@
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import React from 'react';
+// import { RecoilRoot } from 'recoil';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+// import axios from 'axios';
+
+// axios.defaults.baseURL = 'https://www.finble.xyz';
+// axios.defaults.withCredentials = true;
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      {/* <RecoilRoot> */}
+      <App />
+      {/* </RecoilRoot> */}
+    </BrowserRouter>
+  </React.StrictMode>
+);
